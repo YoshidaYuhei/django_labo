c1b0b214278ea42b8917f9196c3d6169a8e2edcd
YoshidaYuhei <yoshidayuhei@gmail.com>
2022-10-26 22:06:05 +0900

    feat:tutorial finished

--- project/project/settings.py
+++ project/project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'labo.apps.LaboConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
